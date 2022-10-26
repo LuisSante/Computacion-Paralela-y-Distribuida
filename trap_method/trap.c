a4072a8fa4f989a783acb1e125c4b8f1ac678509
LuisSante <lsantet@unsa.edu.pe>
2022-10-26 08:02:26 -0500

    Threads

---
+++ trap_method/trap.c
@@ -0,0 +1,14 @@
+
+double Trap( double left_endpt, double right_endpt, int trap_count, double base_len) {
+    double estimate, x;
+    int i;
+    estimate = (f(left_endpt) + f(right_endpt)) / 2.0;
+    for (i = 1; i <= trap_count - 1; i++){
+        x = left_endpt + i*base_len;
+        estimate += f(x);
+    }
+    estimate = estimate*base_len;
+
+    return estimate;
+}
+/* Trap */
